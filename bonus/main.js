b291aa190beafba84365daf9f5ca9779deb9808b
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-02-06 21:50:24 +0100

    bonus 1 completato

--- bonus/main.js
+++ bonus/main.js
@@ -4,114 +4,117 @@ const icons = [
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: ''
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: ''
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: ''
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: ''
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
+		color: ''
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: ''
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: ''
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: ''
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: ''
 	}
 ];
+
+// assegno un colore per ogni tipo di icona, generando un colore casuale
+assignColor(icons);
 
 // genero le icone dentro a container
 const container = document.querySelector('.container');
@@ -136,20 +139,75 @@ select.addEventListener('change', function(){
 
 
 
+
 /* FUNZIONI */
+
+// assegna un colore per tipo di icona
+function assignColor(array) {
+	const animalColor = generateRandomColor();
+	const vegetableColor = generateRandomColor();
+	const userColor = generateRandomColor();
+
+	array.forEach(icon => {
+		switch (icon.type) {
+			case 'animal':
+				icon.color = animalColor;
+				break;
+			case 'vegetable':
+				icon.color = vegetableColor;
+				break;
+			case 'user':
+				icon.color = userColor;
+		}
+	});
+
+	return true;
+}
+
 
 // genera tutte le icone dentro a container, a partire dalla struttura dati icons
 function drawIcons(container, array) {
 	let content = '';
 
 	array.forEach(icon => {
-		content +=`<div class="icon ${icon.color}">
-						<i class="${icon.family} ${icon.prefix}${icon.name}"></i>
+		content +=`<div class="icon">
+						<i style="color:${icon.color};" class="${icon.family} ${icon.prefix}${icon.name}"></i>
 						<span>${icon.name}</span>
 					</div>`;
 	});
 
 	container.innerHTML = content;
+}
+
+
+// genera un colore esadecimale casuale
+function generateRandomColor() {
+	let color = '#';
+
+	for (let i = 0; i < 6; i++) {
+		let hexValue = Math.floor(Math.random() * 16);
+		switch (hexValue) {
+			case 10:
+				hexValue = 'a';
+				break;
+			case 11:
+				hexValue = 'b';
+				break;
+			case 12:
+				hexValue = 'c';
+				break;
+			case 13:
+				hexValue = 'd';
+				break;
+			case 14:
+				hexValue = 'e';
+				break;
+			case 15:
+				hexValue = 'f';
+		}
+		color += hexValue;
+	}
+	return color;
 }
 
 
