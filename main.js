33ab8e51ed1fccb370a730fe2642a79b86214f35
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-02-06 18:41:38 +0100

    pre-bonus completato

---
+++ main.js
@@ -0,0 +1,157 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const container = document.querySelector('.container');
+drawIcons(container, icons);
+
+
+const select = document.getElementById('type-selector');
+
+select.addEventListener('change', function(){
+	if (this.value == '') {
+		drawIcons(container, icons);
+	} else {
+		const filteredIcons = icons.filter(icon => {
+			if (icon.type == this.value) {
+				return true;
+			}
+			return false;
+		});
+		drawIcons(container, filteredIcons);
+	}
+});
+
+
+
+
+
+
+function drawIcons(container, array) {
+	let content = '';
+
+	array.forEach(icon => {
+		content +=`<div class="icon ${icon.color}">
+						<i class="${icon.family} ${icon.prefix}${icon.name}"></i>
+						<span>${icon.name}</span>
+					</div>`;
+	});
+
+	container.innerHTML = content;
+}
+
+
+
+
+
